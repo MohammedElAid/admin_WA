040fdddfc826a9e2351a65a062fd2b5ce771d630
HAMMOU ZERROUKI Mohammed <hzmiky@gmail.com>
2023-09-05 18:59:32 +0200

    02h38

--- admin/components/navbar.tsx
+++ admin/components/navbar.tsx
@@ -1,14 +1,32 @@
-import { UserButton } from '@clerk/nextjs'
 import React from 'react'
+import { UserButton, auth } from '@clerk/nextjs'
+
+import { redirect } from 'next/navigation'
+
+
 import MainNav from '@/components/main-nav'
 import StoreSwitcher from '@/components/store-swither'
 
-const Navbar = () => {
+import prismadb from '@/lib/prismadb'
+
+const Navbar = async () => {
+    const { userId } = auth()
+
+
+    if (!userId) {
+        redirect('/signin')
+    }
+    const store = await prismadb.store.findMany({
+        where: {
+            userId,
+        }
+    })
+
     return (
         <div className='border-b'>
             <div className='flex h-16 items-center px-4'>
 
-                <StoreSwitcher />
+                <StoreSwitcher items={store} />
                 <MainNav className='mx-6' />
                 <div className='ml-auto flex items-center space-x-4'>
                     <UserButton afterSignOutUrl='/' />
