38d3d07cf2072d93be4ec7b3ea16ecfd26e40732
HAMMOU ZERROUKI Mohammed <hzmiky@gmail.com>
2023-09-12 22:41:15 +0200

    03h05

---
+++ admin/app/api/stores/[storeId]/route.ts
@@ -0,0 +1,26 @@
+import { auth } from "@clerk/nextjs"
+import { NextResponse } from "next/server"
+
+export async function PATCH(
+    req: Request,
+    { params }: { params: { storeId: string } }
+) {
+    try {
+        const { userId } = auth()
+        const body = await req.json();
+        const { name } = body;
+
+        if (!userId) {
+            return new NextResponse('Unauthorized', { status: 401 })
+        }
+        if (!name) {
+            return new NextResponse('Name is required', { status: 400 })
+        }
+
+
+    } catch (error) {
+        console.log('[STORE_PATCH]', error)
+        return new NextResponse('Internal error', { status: 500 })
+    }
+
+} 
